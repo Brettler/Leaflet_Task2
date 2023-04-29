7258996294830d17ef5e77590b1d1b82e282a8af
liadb <liad.brettler@live.biu.ac.il>
2023-04-29 12:33:23 +0300

    Starting convert to JSX syntax

---
+++ src/FriendProperties/friends.js
@@ -0,0 +1,18 @@
+import imgRegnar from './images/Ragnar_Lothbrok.jpg'
+import imgLitt from './images/Louis_Litt.jpg'
+import imgEleanor from './images/Eleanor_Shellstrop.jpeg'
+import imgUnderwood from './images/Frank_Underwood.jpg'
+import imgHouse from './images/Gregory_House.jpg'
+import imgSeinfeld from './images/Jerry_Seinfeld.png'
+import imgRick from './images/Rick_Sanchez.jpg'
+{/* Define Object that will contain the freind block parameter */ }
+const friends = [{ img: imgRegnar, name: 'Ragnar Lothbrok', day_time: '13:53 30/04/2023', last_msg: 'WHO WANTS TO BE KING??!!!' },
+                { img: imgLitt, name: 'Louis Litt', day_time: '6:00 19/04/2023', last_msg: 'You just got Litt up!!', num_msg: '217' },
+                { img: imgEleanor, name: 'Eleanor_Shellstrop', day_time: '2:49 21/10/2021', last_msg: "This is the Bad Place isn't it...", num_msg: '6' },
+                { img: imgUnderwood, name: 'Frank Underwood', day_time: '22:27 6/11/2018', last_msg: 'You guessed it, I still hate children.', num_msg: '1' },
+                { img: imgHouse, name: 'Gregory House', day_time: '16:08 13/01/2002', last_msg: 'I cared for eight seconds, then I got distracted.', num_msg: '2' },
+                { img: imgSeinfeld, name: 'Jerry Seinfeld', day_time: '13:30 17/01/1990', last_msg: 'You gotta see the baby!', num_msg: '12' },
+                { img: imgRick, name: 'Rick Sanchez', day_time: '3:18 13/08/1402', last_msg: 'Weddings are basically funerals with cake.' },
+                ];
+
+export default friends;
---
+++ src/SearchFriend/SearchFriend.js
@@ -0,0 +1,26 @@
+import {useRef} from 'react';
+
+function SearchFriend({doSearch}) {
+    {/* We initilaize the varaible with null.*/}
+    const searchBox = useRef(null);
+
+    const search = function() {
+        {/* We send to the doSearch the value that the user type in the search input box.*/}
+        doSearch(searchBox.current.value)
+    }
+    
+    
+    return (
+        <div className="search_chat">
+            <div>
+                {/* Ref={} is the simmilar to do id='' and then getElementById. Ref is a hook -> make for us possible to catch things from the user.
+                     we creat reference to the element input -> we call this refernce searchBox  and then we can use the 
+                     search box everywhere we want in the code, we use it in the function above the recive the current value
+                     that the user typed in the search box.*/}
+                <input ref={searchBox} onKeyUp={search} className="form-control" type="text" placeholder="Search here" aria-label="default input example" />
+                <i id="search_icon" className="bi bi-search" />
+            </div>
+        </div>
+    );
+}
+export default SearchFriend;
