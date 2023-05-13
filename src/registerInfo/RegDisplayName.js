f2fbf759c396c39e75af46ec9a33422c04abcb85
liadb <liad.brettler@live.biu.ac.il>
2023-05-13 17:15:58 +0300

    need to fix time in the messages

--- src/registerInfo/RegDisplayName.js
+++ src/registerInfo/RegDisplayName.js
@@ -1,11 +1,11 @@
 /* In this section, we assign the user's display name, which is a mandatory field without any specific restrictions.  */
-function RegDisplayName({ registerDisplayName, setRegisterDisplayName }) {
+function RegDisplayName({displayNameRef, registerDisplayName, setRegisterDisplayName }) {
 
     return (
         <div className="row mb-3">
             <label htmlFor="inputName" className="col-sm-2 col-form-label">Display Name</label>
             <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputName" value={registerDisplayName}
+                <input ref={displayNameRef} type="text" className="form-control" id="inputName" value={registerDisplayName}
                        onChange={(e) => setRegisterDisplayName(e.target.value)}/>
             </div>
         </div>
--- src/registerInfo/RegUsername.js
+++ src/registerInfo/RegUsername.js
@@ -1,14 +1,24 @@
 /* The username field allows the user to choose their desired username, ensuring that it is not already in use by
 * another user. The username is case-insensitive. */
-function RegUsername({registerUsername, setRegisterUsername, usernameErrorMsg}) {
+function RegUsername({usernameRef, registerUsername, setRegisterUsername, usernameErrorMsg}) {
+    const handleUsernameChange = (e) => {
+        setRegisterUsername(e.target.value);
+        e.target.setCustomValidity(usernameErrorMsg || '');
+    };
 
     return (
         <div className="row mb-3">
-            <label htmlFor="inputEmail3" className="col-sm-2 col-form-label">Username</label>
+            <label htmlFor="inputUserameAPP" className="col-sm-2 col-form-label">Username</label>
             <div className="col-sm-10">
-                <input type="text" className="form-control" id="inputEmail3" value={registerUsername}
-                       onChange={(e) => setRegisterUsername(e.target.value)} required/>
-                {usernameErrorMsg && <div className="alert alert-danger">{usernameErrorMsg}</div>}
+                <input ref={usernameRef} type="text" className={`form-control ${usernameErrorMsg ? 'is-invalid' : ''}`}
+                       id="inputUserameAPP"
+                       value={registerUsername}
+                       onChange={handleUsernameChange}
+                       onInvalid={(e) => e.target.setCustomValidity(usernameErrorMsg || '')}
+                       required/>
+                <div className="invalid-feedback">
+                    {usernameErrorMsg}
+                </div>
             </div>
         </div>
     );
