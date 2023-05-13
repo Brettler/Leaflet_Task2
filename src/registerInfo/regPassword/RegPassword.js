569c5bf98328ac1e5369d45de76d487f70001407
liadb <liad.brettler@live.biu.ac.il>
2023-05-13 18:49:21 +0300

    Finished working on invalid fields

--- src/registerInfo/regPassword/RegPassword.js
+++ src/registerInfo/regPassword/RegPassword.js
@@ -1,44 +1,64 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useCallback } from 'react';
 
 /* In this section, we validate if the entered password matches the verified password. If they do not match,
 * a relevant error message will be displayed. */
-function RegPassword({validPassword, validVerifyPassword, regErrorPasswordMSG, regErrorVerifyPasswordMSG}) {
-    const [verifyPasswordChanged, setVerifyPasswordChanged] = useState(false);
+function RegPassword({passwordRef, verifyPasswordRef,
+                         validPassword, validVerifyPassword,
+                         regErrorPasswordMSG, regErrorVerifyPasswordMSG}) {
+    const [verifyPassword, setVerifyPassword] = useState('');
+
+
+    const handleErrorPassword = (e) => {
+        validPassword(e);
+        e.target.setCustomValidity(regErrorPasswordMSG  || '');
+    };
+
+    const handleErrorVerifyPassword = useCallback((e) => {
+        validVerifyPassword(e);
+        setVerifyPassword(e.target.value);
+        e.target.setCustomValidity(regErrorVerifyPasswordMSG  || '');
+    }, [validVerifyPassword, regErrorVerifyPasswordMSG]);
+
+
+    useEffect(() => {
+        if (verifyPassword) {
+            // Create a mock event object because validVerifyPassword only get events as an input.
+            const event = {
+                target: {
+                    value: verifyPassword
+                }
+            };
+            validVerifyPassword(event);
+        }
+    }, [verifyPassword, validVerifyPassword]);
 
     return (
         <>
-            {/* Password section */}
             <div className="row mb-3">
                 <label htmlFor="inputPassword3" className="col-sm-2 col-form-label">Password</label>
                 <div className="col-sm-10">
-                    <input type="password" className="form-control" id="inputPassword3" onChange={(e) => {
-                        validPassword(e);
-                        if (verifyPasswordChanged) {
-                            validVerifyPassword(e);
-                        }
-                    }}/>
-                    {regErrorPasswordMSG
-                        && (
-                            <div className="alert alert-danger" role="alert">
-                                {regErrorPasswordMSG}
-                            </div>
-                        )}
+                    <input ref={passwordRef} type="password" className={`form-control ${regErrorPasswordMSG ? 'is-invalid' : ''}`}
+                           id="inputPassword3"
+                           onChange={handleErrorPassword}
+                           onInvalid={(e) => e.target.setCustomValidity(regErrorPasswordMSG || '')}
+                           required/>
+                    <div className="invalid-feedback">
+                        {regErrorPasswordMSG}
+                    </div>
                 </div>
             </div>
 
-            {/* Verify password section */}
             <div className="row mb-3">
                 <label htmlFor="inputPassword4" className="col-sm-2 col-form-label">Verify Password</label>
                 <div className="col-sm-10">
-                    <input type="password" className="form-control" id="inputPassword4" onChange={(e) => {
-                        setVerifyPasswordChanged(true);
-                        validVerifyPassword(e);
-                    }}/>
-                    {verifyPasswordChanged && regErrorVerifyPasswordMSG && (
-                        <div className="alert alert-danger" role="alert">
-                            {regErrorVerifyPasswordMSG}
-                        </div>
-                    )}
+                    <input ref={verifyPasswordRef} type="password" className={`form-control ${regErrorVerifyPasswordMSG ? 'is-invalid' : ''}`}
+                           id="inputPassword4"
+                           onChange={handleErrorVerifyPassword}
+                           onInvalid={(e) => e.target.setCustomValidity(regErrorVerifyPasswordMSG || '')}
+                           required/>
+                    <div className="invalid-feedback">
+                        {regErrorVerifyPasswordMSG}
+                    </div>
                 </div>
             </div>
         </>
