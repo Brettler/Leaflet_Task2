93e01726c58c2b546fc6e4a7cc5a6ebcd91a59b9
Edenm0de <eden.berman2@gmail.com>
2023-05-13 13:36:30 +0300

    Finished comments

--- src/profileUser/ProfileUser.js
+++ src/profileUser/ProfileUser.js
@@ -3,17 +3,22 @@ import {Navigate} from "react-router-dom";
 import UserInfo from "./UserInfo";
 import UserSettingOptions from "./UserSettingOptions";
 
+/* This function implements the logout functionality. When the user clicks on the logout icon, they will be redirected
+* to the login page.  */
 function ProfileUser({userInfo, usersRegisterList, setUsersRegisterList}) {
     const [isLoggedIn, setIsLoggedIn] = useState(true);
 
+    // Set user to 'logged out'.
     function handleLogout() {
         setIsLoggedIn(false);
     }
 
+    // Redirect the user to the login page.
     if (!isLoggedIn) {
         return <Navigate to="/"/>;
     }
 
+    // Logout the user.
     return (
         <div className="header">
             <UserInfo userInfo={userInfo}/>
